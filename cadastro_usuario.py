b5fe7ed729cbaae7b77477c0ac07fd63862ed85c
Igor Oliveira Castilho <igorolivercastilho@gmail.com>
2025-09-12 22:24:34 -0300

    Create cadastro_usuario.py

---
+++ cadastro_usuario.py
@@ -0,0 +1,14 @@
+print("=== Cadastro de Usuário ===")
+
+print("Preencha os dados a seguir:  \n")
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+email = input("Email: ")
+cidade = input("Digite a cidade onde mora: ")
+
+print("\n=== FICHA DE USUÁRIO ===")
+print(f"Nome: {nome}")
+print(f"Idade: {idade} anos")
+print(f"Email: {email}")
+print(f"Cidade: {cidade}")
+print("==========================")
